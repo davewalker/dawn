5c8a6cfc3bbbeda8bae73fda5e519c8f551fc4b6
Dave Walker <hello@david-walker.info>
2016-02-22 21:04:54 +1100

    About

---
+++ about.php
@@ -0,0 +1,59 @@
+<?php include 'src/components/head/head.php' ?>
+
+<div class="content content--wide">
+    <?php $include_intro = false; ?>
+    <?php include 'src/components/navigation/navigation.php' ?>
+
+    <div class="col-wrapper bm--x3">
+
+        <div class="col--two">
+            <h1 class="section-heading header--gold uppercase">About Gravita</h1>
+            <h2 class="contact-introduction delta header--white">The art of design crafts a vision worth remembering.</h2>
+        </div>
+
+        <div class="col--two col--last">
+            <p>What do we do exactly…? We work closely with organisations to help
+            reshape, align and accentuate their purpose.</p>
+
+            <p>Our aim is to craft new visual languages from brand development
+            through to product / service which fulfil the right reaction and
+            response. For us, design isn’t result orientated, it’s about the
+            solution, And, beyond all of the above, leaving a lasting impression
+            to be proud of.</p>
+        </div>
+
+    </div>
+
+    <div class="col-wrapper">
+        <div class="col--two col--last">
+            <div class="bm--x3">
+                <h2 class="section-heading header--gold uppercase">Brand Platforms</h2>
+                <p>At the end of the day we’re people talking to people, how we present
+                ourselves means a lot but most of all we must be honest. We don’t
+                believe in gimmics and fluff, so we brand for the internal, this allows
+                us to provide the right tools for a more organic communication through
+                identity, type, color, photography, website and application.</p>
+            </div>
+
+            <div class="bm--x3">
+                <h2 class="section-heading header--gold uppercase">Product Development</h2>
+                <p>We like to surround ourselves with context and narrative, without
+                filtering or assumption we can better understand every challange. From
+                discovery to information architecture (UX) to implementation, we focus
+                on building stories and building better, experiences.</p>
+            </div>
+        </div>
+
+        <div class="col--two">
+            <?php include 'src/components/featured-links/featured-links.php'; ?>
+        </div>
+    </div>
+
+</div>
+
+<aside class="main-content main-content--narrow contact-information fixed-panel--l-only">
+    <?php include 'src/components/contact-information/contact-information.php'; ?>
+</aside>
+
+<div class="clearfix"></div>
+<?php include 'src/components/footer-scripts/footer-scripts.php' ?>
